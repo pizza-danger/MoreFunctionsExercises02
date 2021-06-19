f10dd61da34e61e9a86cba70d7a8d2e85432b778
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-19 14:27:54 +0000

    STARTED

--- index.js
+++ index.js
@@ -6,8 +6,15 @@
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. */
-
-
+//TRIED TO MAKE THIS FUNCTION WITHOUT NAMING IT
+let timesThree = function(n) {
+  if (typeof(n) === NaN) {
+  console.log("ARRR");
+  } else {
+   return n * 3;
+  }
+ };
+console.log(timesThree("dude"));
 
 /* Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
