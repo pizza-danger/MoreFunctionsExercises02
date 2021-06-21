4b370061229ba42552b7cdcede8a1e02fab126ba
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-21 02:50:58 +0000

    Close

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 //Create an anonymous fucntion and set it equal to a variable.
 
 
-
 /* Your function should:
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. */
 //TRIED TO MAKE THIS FUNCTION WITHOUT NAMING IT
-let timesThree = function(n) {
-  if (typeof(n) !== Number) {
-  console.log("ARRR");
-  } else {
-   return n * 3;
-  }
- };
-console.log(timesThree(4));
+
+// let timesThree = function(n) {
+//   if (typeof(n) !== Number) {
+//   console.log("ARRR");
+//   } else {
+//    return n * 3;
+//   }
+//  };
+//console.log(timesThree(4));
+
 
 /* Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
@@ -23,3 +24,17 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+let tripled = arr.map(function (n) {
+  if (typeof(n) !== Number) {
+  console.log("ARRR");
+  } else {
+   return n * 3;
+  }
+});
+
+//let tripled = arr.map(timesThree);
+
+console.log(arr);
+
+console.log(tripled);
